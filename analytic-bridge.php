c885a94d95c5f9edf367af7e8718af0db144a995
zu2 <3023910+zu2@users.noreply.github.com>
2020-10-25 03:37:33 +0900

    merge https://github.com/INN/Google-Analytics-Popular-Posts/pull/68/files

--- analytic-bridge.php
+++ analytic-bridge.php
@@ -189,7 +189,9 @@ function query_and_save_analytics($analytics, $startdate, $verbose=false) {
 		// caching iterator contains hasNext() functionality.
 		$iter = new CachingIterator(new ArrayIterator($report->rows));
 
+		$count = 0;
 		foreach ($iter as $r) {
+			$count++;
 			// $r[0] - pagePath
 			$gapath = $r[0];
 
@@ -225,6 +227,10 @@ function query_and_save_analytics($analytics, $startdate, $verbose=false) {
 				//
 
 			} else {
+				if( $count > 1 ){
+					$pagesql .= ", \n";
+					$metricsql .= ", \n";
+				}
 
 				// Insert into our 'post' table the pagepath and related postid
 				// (if it doesn't exist).
@@ -261,10 +267,6 @@ function query_and_save_analytics($analytics, $startdate, $verbose=false) {
 
 					);
 
-				if($iter->hasNext()) {
-					$pagesql .= ", \n";
-					$metricsql .= ", \n";
-				}
 			}
 		}
 		$pagesql   = rtrim($pagesql,", \n");   // what happen?
